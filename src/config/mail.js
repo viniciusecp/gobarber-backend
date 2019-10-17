d1cfe008ed062a45400e171cc2d801e06da1500c
Vinicius Silva <viniciusfs.ecp@gmail.com>
2019-10-17 14:42:58 -0300

    Finalizado segunda parte do backend da aplicacao gobarber

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'ce0f008001f80d',
-    pass: 'e416df36efae1b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
